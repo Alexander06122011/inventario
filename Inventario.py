883c5f1868f3087df828f964beb2cb793846b8b1
Alexander Villalobos Rivas <alexandervillalobosrivas@gmail.com>
2025-11-04 11:15:09 -0500

    feat: nuevas funcionalidades añadidas al código

--- Inventario.py
+++ Inventario.py
@@ -1,3 +1,15 @@
 def sistema():
-    cantidad = int(input("Ingresa la cantidad de productos:\n"))
-
+    while True:
+        try: 
+            cantidad = int(input("Ingresa la cantidad de productos:\n"))
+            precio = int(input("Ingresa el precio por prducto:\n"))
+            print(f"El valor total es: {cantidad * precio}")
+            break
+        except ValueError:
+            print("Solo se pueden ingresar números")
+        except KeyboardInterrupt:
+            print("Gracias por usar nuestro sistema\n")
+            break
+            
+        
+sistema()
